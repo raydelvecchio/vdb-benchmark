5a52816e9e61ff61ce988cad4abfcd766f2c52e1
Salvatore <96501610+saldelv@users.noreply.github.com>
2024-02-02 20:36:40 -0500

    Added benchmark for Qdrant

---
+++ qdrant_test.py
@@ -0,0 +1,103 @@
+from constants import *
+import time
+import glob
+import os
+import timeit
+from vlite2.utils import chop_and_chunk
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams, PointStruct
+from sentence_transformers import SentenceTransformer
+
+
+def memorize_one_q(q):
+    short_data_embeddings = SentenceTransformer('all-MiniLM-L6-v2').encode(SHORT_DATA).tolist()
+    q.upsert(
+        collection_name="test_collection",
+        points=[PointStruct(id=0, vector=short_data_embeddings)]
+    )
+
+def remember_q(q, text):
+    text_embeddings = SentenceTransformer('all-MiniLM-L6-v2').encode(text).tolist()
+    search_result = q.search(
+        collection_name="test_collection", query_vector=text_embeddings
+    )
+
+def memorize_many_q(q):
+    long_data_chunked = chop_and_chunk(LONG_DATA, 512)
+    long_data_embeddings = SentenceTransformer('all-MiniLM-L6-v2').encode(long_data_chunked).tolist()
+    for i in range(len(long_data_embeddings)):
+        q.upsert(
+            collection_name="test_collection",
+            points=[PointStruct(id=i, vector=long_data_embeddings[i])]
+        )
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    
+    memorize_one_q_time = timeit.timeit('memorize_one_q(qdrant)', 
+                        setup='''
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
+from __main__ import memorize_one_q
+qdrant = QdrantClient(":memory:")
+qdrant.create_collection(
+collection_name="test_collection",
+vectors_config=VectorParams(size=384, distance=Distance.COSINE)
+)
+                        ''',
+                        number=num_executions) / num_executions
+    print("FINISHED MEMORIZE ONE Q")
+
+    remember_one_q_time = timeit.timeit('remember_q(qdrant, "hello")', 
+                        setup='''
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
+from __main__ import memorize_one_q, remember_q
+qdrant = QdrantClient(":memory:")
+qdrant.create_collection(
+collection_name="test_collection",
+vectors_config=VectorParams(size=384, distance=Distance.COSINE)
+)
+memorize_one_q(qdrant)
+                        ''',
+                        number=num_executions) / num_executions
+    print("FINISHED REMEMBER ONE Q")
+
+    memorize_many_q_time = timeit.timeit('memorize_many_q(qdrant)', 
+                        setup='''
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
+from __main__ import memorize_many_q
+qdrant = QdrantClient(":memory:")
+qdrant.create_collection(
+collection_name="test_collection",
+vectors_config=VectorParams(size=384, distance=Distance.COSINE)
+)
+                        ''',
+                        number=num_executions) / num_executions
+    print("FINISHED MEMORIZE MANY Q")
+
+    remember_many_q_time = timeit.timeit('remember_q(qdrant, "civil law")', 
+                        setup='''
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
+from __main__ import memorize_many_q, remember_q
+qdrant = QdrantClient(":memory:")
+qdrant.create_collection(
+collection_name="test_collection",
+vectors_config=VectorParams(size=384, distance=Distance.COSINE)
+)
+memorize_many_q(qdrant)
+                        ''',
+                        number=num_executions) / num_executions
+    print("FINISHED REMEMBER MANY Q")
+
+    print(f"\n\nNumber of executions averaged over: {num_executions}")
+    print(f"q memorize one: {memorize_one_q_time}")
+    print(f"q remember one: {remember_one_q_time}")
+    print(f"q memorize many: {memorize_many_q_time}")
+    print(f"q remember many: {remember_many_q_time}")
+
+    end_time = time.time()
+    print(f"Total time to run: {end_time - start_time} seconds")
