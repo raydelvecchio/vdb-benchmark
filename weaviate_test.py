f06057396c51df53808d323d0cec3fff330975b0
Salvatore <96501610+saldelv@users.noreply.github.com>
2024-02-05 22:06:56 -0500

    Updated weaviate to v4

--- weaviate_test.py
+++ weaviate_test.py
@@ -10,37 +10,34 @@ import pandas as pd
 from openpyxl import load_workbook
 import xlsxwriter
 
-def memorize_one_w(client):
+def memorize_one_w(questions):
     short_data_embeddings = SentenceTransformer('all-MiniLM-L6-v2').encode(SHORT_DATA).tolist()
-    with client.batch as batch:
-        batch.add_data_object(
-            data_object={"test": short_data_embeddings},
-            class_name="Question"
+    with questions.batch.dynamic() as batch:
+        batch.add_object(
+            properties={"test": "id0"},
+            vector=short_data_embeddings
         )
 
-def remember_w(client, text):
-    text = SentenceTransformer('all-MiniLM-L6-v2').encode(text).tolist()
-    response = (
-        client.query
-        .get("Question", ["test"])
-        .with_near_text({"concepts": text})
-        .do()
+def remember_w(questions, text):
+    vector = SentenceTransformer('all-MiniLM-L6-v2').encode(text).tolist()
+    response = questions.query.near_vector(
+        near_vector=vector
     )
 
-def memorize_many_w(client):
+def memorize_many_w(questions):
     long_data_chunked = chop_and_chunk(LONG_DATA, 512)
     long_data_embeddings = SentenceTransformer('all-MiniLM-L6-v2').encode(long_data_chunked).tolist()
-    with client.batch as batch:
-        for i in range(len(long_data_embeddings)):
-            batch.add_data_object(
-                data_object={"test": long_data_embeddings[i]},
-                class_name="Question"
+    with questions.batch.dynamic() as batch:
+        for i in range(len(long_data_chunked)):
+            batch.add_object(
+                properties={"test": "id{i}"},
+                vector=long_data_embeddings[i]
             )
 
 if __name__ == "__main__":
     start_time = time.time()
     
-    memorize_one_w_time = timeit.timeit('memorize_one_w(client)', 
+    memorize_one_w_time = timeit.timeit('memorize_one_w(questions)', 
                         setup='''
 import weaviate
 import weaviate.classes as wvc
@@ -48,21 +45,19 @@ import os
 from dotenv import load_dotenv
 from __main__ import memorize_one_w
 load_dotenv(dotenv_path='.env', verbose=True)
-client = weaviate.Client(
-    url = os.getenv('W_URL'),
-    auth_client_secret=weaviate.auth.AuthApiKey(api_key=os.getenv('W_API_KEY')),
+client = weaviate.connect_to_wcs(
+    cluster_url = os.getenv('W_URL'),
+    auth_credentials=weaviate.auth.AuthApiKey(api_key=os.getenv('W_API_KEY')),
 )
-class_obj = {
-    "class": "Question",
-    "vectorizer": "none",
-}
-client.schema.delete_class("Question")
-client.schema.create_class(class_obj)
+client.collections.delete("Question")
+questions = client.collections.create(
+    name="Question",
+)
                         ''',
                         number=num_executions) / num_executions
     print("FINISHED MEMORIZE ONE W")
     
-    remember_one_w_time = timeit.timeit('remember_w(client, "hello")', 
+    remember_one_w_time = timeit.timeit('remember_w(questions, "hello")', 
                         setup='''
 import weaviate
 import weaviate.classes as wvc
@@ -70,22 +65,20 @@ import os
 from dotenv import load_dotenv
 from __main__ import memorize_one_w, remember_w
 load_dotenv(dotenv_path='.env', verbose=True)
-client = weaviate.Client(
-    url = os.getenv('W_URL'),
-    auth_client_secret=weaviate.auth.AuthApiKey(api_key=os.getenv('W_API_KEY')),
+client = weaviate.connect_to_wcs(
+    cluster_url = os.getenv('W_URL'),
+    auth_credentials=weaviate.auth.AuthApiKey(api_key=os.getenv('W_API_KEY')),
 )
-class_obj = {
-    "class": "Question",
-    "vectorizer": "none",
-}
-client.schema.delete_class("Question")
-client.schema.create_class(class_obj)
-memorize_one_w(client)
+client.collections.delete("Question")
+questions = client.collections.create(
+    name="Question",
+)
+memorize_one_w(questions)
                         ''',
                         number=num_executions) / num_executions
     print("FINISHED REMEMBER ONE W")
 
-    memorize_many_w_time = timeit.timeit('memorize_many_w(client)', 
+    memorize_many_w_time = timeit.timeit('memorize_many_w(questions)', 
                         setup='''
 import weaviate
 import weaviate.classes as wvc
@@ -93,20 +86,19 @@ import os
 from dotenv import load_dotenv
 from __main__ import memorize_many_w
 load_dotenv(dotenv_path='.env', verbose=True)
-client = weaviate.Client(
-    url = os.getenv('W_URL'),
-    auth_client_secret=weaviate.auth.AuthApiKey(api_key=os.getenv('W_API_KEY')),
+client = weaviate.connect_to_wcs(
+    cluster_url = os.getenv('W_URL'),
+    auth_credentials=weaviate.auth.AuthApiKey(api_key=os.getenv('W_API_KEY')),
 )
-class_obj = {
-    "class": "Question",
-    "vectorizer": "none",
-}
-client.schema.delete_class("Question")
-client.schema.create_class(class_obj)
+client.collections.delete("Question")
+questions = client.collections.create(
+    name="Question",
+)
                         ''',
                         number=num_executions) / num_executions
+    print("FINISHED MEMORIZE MANY W")
 
-    remember_many_w_time = timeit.timeit('remember_w(client, "civil law")', 
+    remember_many_w_time = timeit.timeit('remember_w(questions, "civil law")', 
                         setup='''
 import weaviate
 import weaviate.classes as wvc
@@ -114,17 +106,15 @@ import os
 from dotenv import load_dotenv
 from __main__ import memorize_many_w, remember_w
 load_dotenv(dotenv_path='.env', verbose=True)
-client = weaviate.Client(
-    url = os.getenv('W_URL'),
-    auth_client_secret=weaviate.auth.AuthApiKey(api_key=os.getenv('W_API_KEY')),
+client = weaviate.connect_to_wcs(
+    cluster_url = os.getenv('W_URL'),
+    auth_credentials=weaviate.auth.AuthApiKey(api_key=os.getenv('W_API_KEY')),
 )
-class_obj = {
-    "class": "Question",
-    "vectorizer": "none",
-}
-client.schema.delete_class("Question")
-client.schema.create_class(class_obj)
-memorize_many_w(client)
+client.collections.delete("Question")
+questions = client.collections.create(
+    name="Question",
+)
+memorize_many_w(questions)
                         ''',
                         number=num_executions) / num_executions
     print("FINISHED REMEMBER MANY W")
@@ -132,15 +122,15 @@ memorize_many_w(client)
     print(f"\n\nNumber of executions averaged over: {num_executions}")
     print(f"w memorize one: {memorize_one_w_time}")
     print(f"w remember one: {remember_one_w_time}")
-    print(f"w mmemorize many: {memorize_many_w_time}")
+    print(f"w memorize many: {memorize_many_w_time}")
     print(f"w remember many: {remember_many_w_time}\n\n")
 
     load_dotenv(dotenv_path='.env', verbose=True)
-    client = weaviate.Client(
-    url = os.getenv('W_URL'),
-    auth_client_secret=weaviate.auth.AuthApiKey(api_key=os.getenv('W_API_KEY')),
-    )
-    client.schema.delete_class("Question")
+    client = weaviate.connect_to_wcs(
+    cluster_url = os.getenv('W_URL'),
+    auth_credentials=weaviate.auth.AuthApiKey(api_key=os.getenv('W_API_KEY')),
+        )
+    client.close()
     
     end_time = time.time()
     print(f"Total time to run: {end_time - start_time} seconds")
